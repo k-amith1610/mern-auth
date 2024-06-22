6143dd67e33eca681036f9061052855dfa340a72
k-amith1610 <2110030372@klh.edu.in>
2024-06-22 19:55:50 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import userRoute from "./routes/user.route.js"; 
 dotenv.config();
 
 const app = express();
 const port = process.env.port || 5000
 const uri = process.env.MONGODB_URL;
+
+
+app.use("/api/user", userRoute)
+
 
 const start = () => {
     try {
